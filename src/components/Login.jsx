ff0b7a5556363874560ee62fd7e1119e70dcae88
ABHI <abhisreekrishna742@gmail.com>
2023-06-26 13:48:08 +0530

    final commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,7 @@
 import React, { useState }  from 'react'
 import '../styles/logIn.css'
+import { Typography } from '@mui/material';
+import {Link} from 'react-router-dom'
 
 function Login() 
 {
@@ -21,20 +23,27 @@ function Login()
          
 
            <div className="form-group">
-            <div className='mb-4'>
-            <label>Email</label>
+             <div className='mb-4'>
               <input type='email'value={email} placeholder='Enter your email id/username' onChange={(e)=> setEmail(e.target.value)} required size={30}></input>
-            </div>
+             </div>
          
-           <div className='mb-4'>
-            <label>Password</label>
-            <input type='password' value={pass} placeholder='Entery your password'onChange={(e)=> setPass(e.target.value)} required size={30}></input>
-           </div>
+             <div className='mb-4'>
+               <input type='password' value={pass} placeholder='Entery your password'onChange={(e)=> setPass(e.target.value)} required size={30}></input>
+             </div>
 
-           <div class="row mb-4">
-            <div class="col-sm-6 offset-sm-3">
-             <button type="submit" class="btn btn-primary login-button">Login</button>
-            </div>
+              <div class="row mb-4">
+                <div class="col-sm-6 offset-sm-3">
+                  <button type="submit" class="btn btn-primary login-button">Login</button>
+                 </div>
+              </div>
+
+              <div className='' style={{"marginLeft":"2em"}}>
+                <Typography variant='body'>
+                  Don't Have An Account ?{' '}
+    <                  Link to='/signup' style={{"marginLeft":"0.5em","textDecoration":"none"}}>
+                     SignUp Here
+                    </Link>
+                      </Typography>
            </div>
 
           </div>
