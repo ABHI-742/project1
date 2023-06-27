972b81042693e9f1558c665a82f96b963475c787
Avishekh1 <avishekh.thakur@zemosolabs.com>
2023-06-28 00:49:52 +0530

    Login Component developed by Deva

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,7 @@
 import React, { useState }  from 'react'
-import HelmetExport, {Helmet} from "react-helmet"
+import '../styles/logIn.css'
+import { Typography } from '@mui/material';
+import {Link} from 'react-router-dom'
 
 function Login() 
 {
@@ -13,18 +15,38 @@ function Login()
    }
 
   return (
-      <div className='div-1'>
-      <Helmet>
-             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      </Helmet>
-        <form onSubmit={handlesubmit} className='form-1'>
-          <h2>Login</h2>
-          <label>email </label>
-          <input type='email'value={email} placeholder='enter your email id/username' onChange={(e)=> setEmail(e.target.value)} required></input>
-  
-          <label>password </label>
-          <input type='password' value={pass} placeholder='entery your password'onChange={(e)=> setPass(e.target.value)} required></input>
-          <button type='sumbit'>Submit</button>
+      <div className='container d-flex center-div justify-content-center align-items-center flex-column'>
+   
+         <div  className='text-white fs-1 mt-5'>
+           <Typography variant='h3'>LogIn</Typography>
+         </div>
+       <form onSubmit={handlesubmit} className='form-1'>
+
+           <div className="form-group mt-5">
+             <div className='mb-4'>
+              <input type='email'value={email} placeholder='Email Id' onChange={(e)=> setEmail(e.target.value)} required size={30}></input>
+             </div>
+         
+             <div className='mb-4'>
+               <input type='password' value={pass} placeholder='Password'onChange={(e)=> setPass(e.target.value)} required size={30}></input>
+             </div>
+
+              <div class="row mb-4">
+                <div class="col-sm-6 offset-sm-3">
+                  <button type="submit" class="btn btn-primary login-button">Login</button>
+                 </div>
+              </div>
+
+              <div className='' style={{"marginLeft":"2em"}}>
+                <Typography variant='body'>
+                  Don't Have An Account ?{' '}
+    <                  Link to='/signup' style={{"marginLeft":"0.5em","textDecoration":"none"}}>
+                     SignUp Here
+                    </Link>
+                      </Typography>
+           </div>
+
+          </div>
         </form>
       </div>
   )
